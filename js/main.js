2e7b635e028d194ae79bae23d18e7c9a0cb72eb6
Gitty Zazon <gittyzazon@gmail.com>
2018-08-09 21:38:49 +0300

    Fix image alt text and rest name semantic

--- js/main.js
+++ js/main.js
@@ -163,9 +163,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  img.alt = 'restaurant image';
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
