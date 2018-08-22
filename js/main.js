44a15aaf7c4f491afcfbe247fef347fa3b0fd742
Gitty Zazon <gittyzazon@gmail.com>
2018-08-22 09:34:31 +0300

    Edit image alt typo and dynamic alt text

--- js/main.js
+++ js/main.js
@@ -163,7 +163,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  img.alt = 'restaurant image';
+  image.alt = restaurant.name;
   li.append(image);
 
   const name = document.createElement('h2');
